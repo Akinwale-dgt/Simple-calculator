7f1ec8c8173e1de78219360c3a201981821fac31
Akinwale-dgt <okunolaakinwale@gmail.com>
2021-05-03 17:21:35 +0100

    first commit

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+def add_numbers(n1, n2,):
+    result = n1 + n2
+    print("the sum is", result)
+
+number1 = 10
+number2 = 15
+add_numbers(number1, number2)
---
+++ product.py
@@ -0,0 +1,8 @@
+def product_numbers(n1, n2, n3):
+    result = n1 * n2 * n3
+    print(result)
+
+number1 = 10
+number2 = 15
+number3 = 2
+product_numbers(number1, number2, number3)
